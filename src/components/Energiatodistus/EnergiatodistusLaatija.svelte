c94c2c7a103bf4d49ab0a21294f199b315747f0a
Toni Pulkkinen <toni.pulkkinen@solita.fi>
2021-02-23 10:00:41 +0200

    AE-920: make laatija as link on energiatodistus form and show energiatodistukset by laatija

---
+++ src/components/Energiatodistus/EnergiatodistusLaatija.svelte
@@ -0,0 +1,27 @@
+<script>
+  import * as R from 'ramda';
+  import { _ } from '@Language/i18n';
+  import * as Maybe from '@Utility/maybe-utils';
+
+  import * as Kayttajat from '@Utility/kayttajat';
+  import Link from '@Component/Link/Link';
+
+  export let energiatodistus;
+  export let whoami;
+
+  const laatijaFullName = R.compose(
+    Maybe.orSome(''),
+    R.prop('laatija-fullname')
+  )(energiatodistus);
+  const laatijaId = R.compose(
+    Maybe.orSome(''),
+    R.prop('laatija-id')
+  )(energiatodistus);
+</script>
+
+<label>{$_('energiatodistus.laatija-fullname')}</label>
+{#if Kayttajat.isPaakayttaja(whoami)}
+  <Link text={laatijaFullName} href={`#/kayttaja/${laatijaId}`} />
+{:else}
+  <div>{laatijaFullName}</div>
+{/if}

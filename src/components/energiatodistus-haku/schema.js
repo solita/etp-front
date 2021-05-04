c29764968d16d3b52a754081bcadc6b82bf4c273
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2021-05-04 09:02:48 +0300

    AE-1270 Add more string absence searches
    Keskeiset suositukset and Lisämerkintöjä were missing their "Ei
    sisällä" search operators.

--- src/components/energiatodistus-haku/schema.js
+++ src/components/energiatodistus-haku/schema.js
@@ -327,8 +327,8 @@ const perustiedot = {
   rakennusosa: [...stringComparisons],
   havainnointikaynti: [havainnointikayntiEquals, havainnointikayntiBetween],
   kieli: [luokitteluEquals(OPERATOR_TYPES.KIELISYYS)],
-  'keskeiset-suositukset-fi': [stringContains],
-  'keskeiset-suositukset-sv': [stringContains]
+  'keskeiset-suositukset-fi': [...stringComparisons],
+  'keskeiset-suositukset-sv': [...stringComparisons]
 };
 
 const lahtotiedot = {
@@ -626,8 +626,8 @@ export const schema = {
   'toteutunut-ostoenergiankulutus': toteutunutOstoenergiankulutus,
   huomiot,
   versio: [versioEquals],
-  'lisamerkintoja-fi': [stringContains],
-  'lisamerkintoja-sv': [stringContains],
+  'lisamerkintoja-fi': [...stringComparisons],
+  'lisamerkintoja-sv': [...stringComparisons],
   laskuriviviite: [...stringComparisons],
   'laatija-id': [laatijaEquals]
 };
